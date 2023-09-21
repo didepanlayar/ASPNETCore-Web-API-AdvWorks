7f67f6fa811361970a3a495cd4c50d97e6962382
Di Depan Layar <albinmaulana@yahoo.com>
2023-09-21 12:21:04 +0700

    Feature: Add Multiple Response Type to Product IAction Controller

--- Controllers/ProductIActionController.cs
+++ Controllers/ProductIActionController.cs
@@ -9,6 +9,8 @@ namespace AdvWorksAPI.Controllers;
 public class ProductIActionController : ControllerBase
 {
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult Get()
     {
         IActionResult ret;
@@ -30,6 +32,8 @@ public class ProductIActionController : ControllerBase
     }
 
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult Get(int id)
     {
         IActionResult ret;
