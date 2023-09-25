5025d4c946ab8149fee2616e327aac3b45137d00
Di Depan Layar <albinmaulana@yahoo.com>
2023-09-25 12:33:09 +0700

    Feature: Implement for Logging Objects

--- Controllers/LogTestController.cs
+++ Controllers/LogTestController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using AdvWorksAPI.EntityLayer;
+using System.Text.Json;
 
 namespace AdvWorksAPI.Controllers;
 
@@ -20,7 +22,17 @@ public class LogTestController : ControllerBase
         // Write Log Messages
         WriteLogMessages();
 
-        return "Check Console Window.";
+        return "Check Console Window or Log File.";
+    }
+
+    [HttpGet]
+    [Route("LogObject")]
+    public string LogObject()
+    {
+        // Write Product Object to Log
+        LogProduct();
+        
+        return "Check Console Window or Log File";
     }
 
     private void WriteLogMessages()
@@ -33,5 +45,24 @@ public class LogTestController : ControllerBase
         _Logger.LogError("This is an Error log entry.");
         _Logger.LogError(new ApplicationException("This is an exception."), "Exception Object.");
         _Logger.LogCritical("This is a Critical log entry.");
+    }
+
+    private void LogProduct()
+  {
+        // Log an Object
+        Product entity = new()
+        {
+            ProductID = 999,
+            Name = "A Test Product",
+            StandardCost = 5M,
+            ListPrice = 10.99M,
+            Color = "Black",
+            Size = "LG",
+            ProductNumber = "TEST001"
+        };
+
+        string json = JsonSerializer.Serialize<Product>(entity);
+
+        _Logger.LogInformation("Product = {json}", json);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 using AdvWorksAPI.EntityLayer;
 using AdvWorksAPI.Interfaces;
 using AdvWorksAPI.RepositoryLayer;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<AdvWorksAPIDefaults, AdvWorksAPIDefaults>();
 builder.Services.AddScoped<IRepository<Product>, ProductRepository>();
+
+// Configure logging to Console and File using Serilog
+builder.Host.UseSerilog((ctx, lc) =>
+{
+    // Log to Console
+    lc.WriteTo.Console();
+    // Log to Rolling File
+    lc.WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
