b2effabc41dd354bf0ae569dd67ce2aa42d64265
Di Depan Layar <albinmaulana@yahoo.com>
2023-09-29 19:22:51 +0700

    Feature: Using Attributes to Affect JSON Serialization

--- EntityLayer/Product.cs
+++ EntityLayer/Product.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace AdvWorksAPI.EntityLayer;
 
 public class Product
@@ -12,17 +14,21 @@ public class Product
 
     public int ProductID { get; set; }
     public string Name { get; set; }
+    [JsonPropertyOrder(-1)]
     public string ProductNumber { get; set; }
     public string? Color { get; set; }
     public decimal StandardCost { get; set; }
     public decimal ListPrice { get; set; }
     public string? Size { get; set; }
     public decimal? Weight { get; set; }
+    [JsonPropertyName("categoryId")]
     public int ProductCategoryID { get; set; }
+    [JsonPropertyName("modelId")]
     public int ProductModelID { get; set; }
     public DateTime SellStartDate { get; set; }
     public DateTime? SellEndDate { get; set; }
     public DateTime? DiscontinuedDate { get; set; }
+    [JsonIgnore]
     public Guid rowguid { get; set; }
     public DateTime ModifiedDate { get; set; }
 }
