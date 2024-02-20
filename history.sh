4b4b8cd38b8522f48f3c15a2be507ab75f318f3d
Di Depan Layar <albinmaulana@yahoo.com>
2024-02-20 20:35:05 +0700

    Feature: Restore AdventureWorksLT Database

--- history.sh
+++ history.sh
@@ -4,3 +4,16 @@ dotnet add package Serilog.Sinks.File;
 dotnet add package System.IdentityModel.Tokens.Jwt;
 dotnet add package Microsoft.AspNetCore.Authentication.JwtBearer --version 6.0.14;
 dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 7.0.3;
+docker cp Databases/AdventureWorksLT.bak mssql:/var/opt/mssql/data;
+### Restore from Microsoft SQL with DBeaver
+# RESTORE FILELISTONLY FROM DISK = '/var/opt/mssql/data/AdventureWorksLT.bak'
+# GO
+
+# RESTORE DATABASE data_adventureworkslt
+# FROM DISK = '/var/opt/mssql/data/AdventureWorksLT.bak'
+# WITH 
+# 	MOVE 'AdventureWorksLT_Data' TO '/var/opt/mssql/data/AdventureWorksLT_Data.mdf',
+# 	MOVE 'AdventureWorksLT_Log' TO '/var/opt/mssql/data/AdventureWorksLT_Log.ldf'
+# GO
+
+# SELECT name FROM sys.databases; 
