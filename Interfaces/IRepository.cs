c954274c53e0a65d96f7e66b7e6be84c677f3e3e
Di Depan Layar <albinmaulana@yahoo.com>
2024-02-21 11:17:18 +0700

    Feature: Add Methods to the Interface and Repository Class

--- Interfaces/IRepository.cs
+++ Interfaces/IRepository.cs
@@ -4,4 +4,8 @@ public interface IRepository<T>
 {
     List<T> Get();
     T? Get(int id);
+    T Insert(T entity);
+    T Update(T entity);
+    T SetValues(T current, T changes);
+    bool Delete(int id);
 }
--- RepositoryLayer/ProductRepository.cs
+++ RepositoryLayer/ProductRepository.cs
@@ -38,4 +38,51 @@ public class ProductRepository : IRepository<Product>
         return _DbContext.Products.Where(row => row.ProductID == id).FirstOrDefault();
     }
     #endregion
+
+    #region Insert Method
+    public Product Insert(Product entity)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+
+    #region Update Method
+    public Product Update(Product entity)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+
+    #region SetValues Method
+    public Product SetValues(Product current, Product changes)
+    {
+        // Since we don't necessarily pass in all the data,
+        // overwrite the changed properties in the one
+        // read from the database
+        // TODO: Make this a little more bullet-proof
+        current.Name = string.IsNullOrWhiteSpace(changes.Name) ? current.Name : changes.Name;
+        current.ProductNumber = string.IsNullOrWhiteSpace(changes.ProductNumber) ? current.ProductNumber : changes.ProductNumber;
+        current.Color = string.IsNullOrWhiteSpace(changes.Color) ? current.Color : changes.Color;
+        current.StandardCost = changes.StandardCost == 0 ? current.StandardCost : changes.StandardCost;
+        current.ListPrice = changes.ListPrice == 0 ? current.ListPrice : changes.ListPrice;
+        current.Size = string.IsNullOrWhiteSpace(changes.Size) ? current.Size : changes.Size;
+        current.Weight = changes.Weight == 0 ? current.Weight : changes.Weight;
+        current.SellStartDate = changes.SellStartDate == DateTime.MinValue ? current.SellStartDate : changes.SellStartDate;
+        current.SellEndDate = changes.SellEndDate == DateTime.MinValue || changes.SellEndDate == null ? current.SellEndDate : changes.SellEndDate;
+        current.DiscontinuedDate = changes.DiscontinuedDate == DateTime.MinValue || changes.DiscontinuedDate == null ? current.DiscontinuedDate : changes.DiscontinuedDate;
+        current.RowGuid = changes.RowGuid == Guid.NewGuid() ? current.RowGuid : Guid.NewGuid();
+        current.ProductCategoryID = changes.ProductCategoryID == 0 ? current.ProductCategoryID : changes.ProductCategoryID;
+        current.ProductModelID = changes.ProductModelID == 0 ? current.ProductModelID : changes.ProductModelID;
+        current.ModifiedDate = DateTime.Now;
+
+        return current;
+    }
+    #endregion
+
+    #region Delete Method
+    public bool Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
 }
