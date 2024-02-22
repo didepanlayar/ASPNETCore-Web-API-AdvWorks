131807bb706b4f3d4d630204a8783cc4a7f38d48
Di Depan Layar <albinmaulana@yahoo.com>
2024-02-22 10:28:50 +0700

    Feature: Validating Product Data Using Data Anotations

--- EntityLayer/Product.cs
+++ EntityLayer/Product.cs
@@ -19,22 +19,29 @@ public partial class Product
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int ProductID { get; set; }
     [Required(ErrorMessage = "The Product Name is required")]
+    [StringLength(50, MinimumLength = 4)]
     public string Name { get; set; }
     [Required]
+    [StringLength(25, MinimumLength = 3)]
     public string ProductNumber { get; set; }
     public string? Color { get; set; }
     [Required]
     [Column(TypeName = "decimal(18, 2)")]
+    [Range(0.01, 9999)]
     public decimal StandardCost { get; set; }
     [Required]
+    [Range(0.01, 9999)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal ListPrice { get; set; }
+    [StringLength(5)]
     public string? Size { get; set; }
     [Column(TypeName = "decimal(8, 2)")]
+    [Range(0.05, 2000)]
     public decimal? Weight { get; set; }
     public int ProductCategoryID { get; set; }
     public int ProductModelID { get; set; }
     [Required]
+    [Range(typeof(DateTime), "2020-01-01", "2030-12-31")]
     public DateTime SellStartDate { get; set; }
     public DateTime? SellEndDate { get; set; }
     public DateTime? DiscontinuedDate { get; set; }
